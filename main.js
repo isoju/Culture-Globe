5fd66bb4b0551eb193907efa98ab430cdf2e2f76
su <adsu@uci.edu>
2021-12-21 01:17:00 -0800

    update

--- main.js
+++ main.js
@@ -6,20 +6,24 @@ import fragmentShader from './shaders/fragment.glsl'
 import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
 import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
 
+const canvasContainer = document.querySelector('#canvasContainer')
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
   75, 
-  innerWidth/innerHeight,
+  canvasContainer.offsetWidth/ canvasContainer.offsetHeight,
   0.1,
   1000
 )
 
 const renderer = new THREE.WebGLRenderer({
-  antialias: true
+  antialias: true,
+  canvas: document.querySelector('canvas')
 })
-renderer.setSize(innerWidth, innerHeight)
+
+renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight)
 renderer.setPixelRatio(window.devicePixelRatio)
-document.body.appendChild(renderer.domElement)
+// document.body.appendChild(renderer.domElement)
 
 //create a sphere
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50),
